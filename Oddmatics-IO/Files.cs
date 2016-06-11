90f7bfab1ddb39468edfde228d3d84aca832baed
rozniak <roryfewell@hotmail.com>
2016-06-11 02:32:29 +0100

    Correcting header
    No important code changes

--- Oddmatics-IO/Files.cs
+++ Oddmatics-IO/Files.cs
@@ -3,7 +3,6 @@
  *
  * This source-code is part of the Oddmatics IO Utilities library by rozza of Oddmatics:
  * <<http://www.oddmatics.uk>>
- * <<http://roz.world>>
  * <<http://github.com/rozniak/Oddmatics-IO>>
  *
  * Sharing, editing and general licence term information can be found inside of the "LICENCE.MD" file that should be located in the root of this project's directory structure.
